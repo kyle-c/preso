738f865785fa5fbbdaccacfd4090d28f43a2446b
Kyle Cooney <homeworld@Kyles-MacBook-Air-2.local>
2025-12-23 14:26:43 -0800

    Initial commit - Chat UX prototype

---
+++ app/page.tsx
@@ -0,0 +1,9 @@
+import { WhatsAppChat } from "@/components/whatsapp-chat"
+
+export default function Home() {
+  return (
+    <main className="h-screen w-full">
+      <WhatsAppChat />
+    </main>
+  )
+}
